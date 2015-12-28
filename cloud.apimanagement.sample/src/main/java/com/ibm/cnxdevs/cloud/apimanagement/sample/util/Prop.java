b442de72ff9d5a5dac1ea0e4d5dc08fb35d884ca
Manish Kataria <mkataria@in.ibm.com>
2015-12-28 12:27:16 +0530

    Adding properties file.

---
+++ cloud.apimanagement.sample/src/main/java/com/ibm/cnxdevs/cloud/apimanagement/sample/util/Prop.java
@@ -0,0 +1,11 @@
+package com.ibm.cnxdevs.cloud.apimanagement.sample.util;
+
+public class Prop {
+	
+	public static String username = "update your username here";
+	public static String password = "update your password here";
+	public static String appkey = "enter your appkey here";
+	
+	public static String serverUrl = "https://apps.na.collabserv.com";
+
+}
