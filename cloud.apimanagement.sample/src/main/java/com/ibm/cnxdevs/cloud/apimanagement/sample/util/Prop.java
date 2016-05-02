a945ee6dc73b59c6c25e39519615b501d8127d25
ritikaKaushal <ritikaus@in.ibm.com>
2016-05-02 11:03:35 +0530

    Deleting entry for app key We are not consuming app key .. Was left by mistake

--- cloud.apimanagement.sample/src/main/java/com/ibm/cnxdevs/cloud/apimanagement/sample/util/Prop.java
+++ cloud.apimanagement.sample/src/main/java/com/ibm/cnxdevs/cloud/apimanagement/sample/util/Prop.java
@@ -4,8 +4,7 @@ public class Prop {
 	
 	public static String username = "update your username here";
 	public static String password = "update your password here";
-	public static String appkey = "enter your appkey here";
-	
+		
 	public static String serverUrl = "https://apps.na.collabserv.com";
 	public static String apiUrl = "/activities/service/atom2/everything";
 	
